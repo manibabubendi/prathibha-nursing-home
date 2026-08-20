870451dceb983ae1079f1698fe42523f478ec4f0
Mani Babu <manibabubendi@gmail.com>
2026-08-20 15:09:26 +0530

    Fix server listen syntax

--- backend/server.js
+++ backend/server.js
@@ -69,4 +69,4 @@ app.get("/", (request, response) => {
     response.sendFile(path.join(__dirname, "..", "index.html"));
 });
 
-app.listen(port, '0.0.0.0' () => console.log(`Prathibha Nursing Home website is running at http://localhost:${port}`));
+app.listen(port, '0.0.0.0', () => console.log(`Prathibha Nursing Home website is running at http://localhost:${port}`));
